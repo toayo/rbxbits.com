e87f9dabfe3d585eb2ccd9e005042029d95ebc93
jub0t <jforeverything2007@gmail.com>
2024-04-04 18:50:45 +0200

    Update

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
 require("dotenv").config()
+
+const NotFound = require("./routes/not_found")
+const express = require("express")
+const app = express()
+
+
+app.use(NotFound)
+
+app.listen(parseInt(process.env.PORT), function (err) {
+    if (err != null) {
+        console.log(`Backend Server Failed To Start: ${err}`)
+        process.exit()
+    }
+
+    console.log(`Started At http://127.0.0.1:${process.env.PORT}`)
+})
---
+++ backend/routes/not_found.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const route = Router();
+
+route.use("*", function (req, res) {
+    const incoming_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    return res.json({
+        success: false,
+        message: `Driver [${incoming_ip}]: Method [${req.method?.toUpperCase()}] - Route ${req.baseUrl} Not Located On This Server`
+    })
+})
+
+module.exports = route
