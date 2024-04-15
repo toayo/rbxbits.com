3d5e228aae13603e063ccee3871511c06223820a
Jareer <jforeverything2007@gmail.com>
2024-04-15 20:04:28 +0200

    Update

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,9 +4,10 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div className="">
-                <div className="p-4 px-5 bg-gradient-to-tl from-first-400 to-first-700">
-                    <span>R$ <span>{new Intl.NumberFormat("en-US").format(13144)}</span></span>
-                </div>
+                <section className="p-5 abeezee flex flex-wrap space-x-3">
+                    <div className="p-8 px-12 flex flex-wrap justify-center items-center rounded-md text-white rounded-md text-white w-3/12 bg-gradient-to-tl from-first-500 to-pink-700">
+                    </div>
+                </section>
             </div>
         );
     }
