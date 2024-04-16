e5f3a154c46c9ba9ed24a41a32dc1477feb6c8d6
Jareer <jforeverything2007@gmail.com>
2024-04-16 22:23:47 +0200

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,6 +6,7 @@ export default class HomePage extends React.Component {
             <div className="">
                 <section className="p-5 abeezee flex flex-wrap space-x-3">
                     <div className="p-8 px-12 flex flex-wrap justify-center items-center rounded-md text-white rounded-md text-white w-3/12 bg-gradient-to-tl from-first-500 to-pink-700">
+
                     </div>
                 </section>
             </div>
